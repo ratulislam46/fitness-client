3b2721cb568d1ae28b3d0bce931384a53bfcf80e
ratulislam46 <arfanratul46@gmail.com>
2025-11-11 20:34:05 +0600

    Modify banner image

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import BannerText from './BannerText';
 
 const Banner = () => {
   return (
-    <section className="relative overflow-hidden py-12 md:py-14 lg:py-16 xl:py-18">
-      {/* Background Image with Blur */}
+    <section className="relative overflow-hidden py-16 md:py-20 lg:py-24 xl:py-28">
+      {/* Background Image */}
       <div
-        className="absolute inset-0 z-0 bg-cover bg-center filter blur-xs"
+        className="absolute inset-0 z-0 bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: "url('https://i.postimg.cc/63XLMWby/ambitious-studio-rick-barrett-1-RNQ11-ZODJM-unsplash.jpg')" }}
       ></div>
 
-      {/* Optional Animated Overlay */}
-      <div className="absolute inset-0 z-0 bg-black/30"></div> {/* dark overlay for text contrast */}
+      {/* Gradient Overlay for better text visibility */}
+      <div className="absolute inset-0 z-0 bg-gradient-to-r from-black/70 to-black/30"></div>
 
-      <div>
+      {/* Content Container */}
+      <div className="container mx-auto relative z-10">
         <BannerText />
       </div>
     </section>
